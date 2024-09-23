e275730de07874f3be06099bff9e66ab5a30cb64
minjae1277 <22073@dongdaemun.ms.kr>
2024-09-23 16:15:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ st.text('우진이 팬들을 위한 팬클럽을 만들어 보았습니다.')
 st.markdown('이곳은 **우진이의 신기 발랄한 사진들을 여러분게 지원**합니다.')
 # 컬러코드: blue, green, orange, red, violet
 st.markdown("우진이의 색상을 :green[초록색]으로, 그리고 **:blue[파란색]** 볼트체로 설정할 수 있습니다.")
-st.markdown("여러분이 우진이를 만날 확률은 :green[$\sqrt{x^2+y^2}=1$]와 같이 latex 문법의 수식 표현도 가능합니다 :pencil:")
+st.markdown("여러분이 우진이를 만날 확률은 :green[$\sqrt{x^2+y^2}=1$]와 같고,충분히 가능합니다 :pencil:")
 
 #LaTex 수식 지원
 #복잡한 수학공식, 기호 등을 웹 페이지에서 깔끔한 수식으로 변환
