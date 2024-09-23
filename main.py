411b861af26082e70ec78ae2a46d07a931c9cfc2
minjae1277 <22073@dongdaemun.ms.kr>
2024-09-23 16:01:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -28,3 +28,12 @@ st.code(sample_code, language="python")
  # 일반 텍스트
 st.text('일반적인 텍스트를 입력해 보았습니다.')
            
+# 마크다운 문법 지원
+ st.markdown('streamlit은 **마크다운 문법을 지원**합니다.')
+# 컬러코드: blue, green, orange, red, violet
+st.markdown("텍스트의 색상을 :green[초록색]으로, 그리고 **:blue[파란색]** 볼트체로 설정할 수 있습니다.")
+st.markdown(":green[$\sqrt{x^2+y^2}=1$]와 같이 latex 문법의 수식 표현도 가능합니다 :pencil:")
+
+#LaTex 수식 지원
+#복잡한 수학공식, 기호 등을 웹 페이지에서 깔끔한 수식으로 변환
+st.latex(r'\sqrt{x^2+y^2}=1')
