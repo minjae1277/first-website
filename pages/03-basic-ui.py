9aab6545b80ac1ecf7c1d7bddbc58cc08139d01b
minjae1277 <22073@dongdaemun.ms.kr>
2024-09-30 15:41:39 +0900

    Update 03-basic-ui.py

--- pages/03-basic-ui.py
+++ pages/03-basic-ui.py
@@ -16,7 +16,7 @@ if button:
 
 #파일 다운로드 버튼
 #샘플 데이터 생성
-# Dateeframe이란, pandas라이브러리에서 제공하는 2차원 데이터  구조(액셀과 유사)
+# Dateframe이란, pandas라이브러리에서 제공하는 2차원 데이터  구조(액셀과 유사)
 dataframe = pd.Dataframe({
   'first column': ['kor''eng''math''science'],
   'second column': [10,20,30,40]
@@ -26,6 +26,6 @@ dataframe = pd.Dataframe({
 st.download_button(
     label='CSV로 성적표 다운로드',
     data=dataframe.to_csv(),   # dataframe을 csv 형태로 변환
-    file_name_='sample.csv',
+    file_name='sample.csv',
     mime='text/csv'            #데이터 유형
 )
