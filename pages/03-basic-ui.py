4f76235d9e1bc3f8705f662fabfd34ae472c0287
minjae1277 <22073@dongdaemun.ms.kr>
2024-09-30 15:36:15 +0900

    Update 03-basic-ui.py

--- pages/03-basic-ui.py
+++ pages/03-basic-ui.py
@@ -6,4 +6,26 @@ import datetime
 # 버튼 클릭
 button = st.button('버튼을 눌러보세요')
 if button:
-  st.write(':blue[버튼}이 눌렸습니다 :sparkles:')
+  st.write(':blue[버튼]이 눌렸습니다 :sparkles:')
+
+
+
+
+
+
+
+#파일 다운로드 버튼
+#샘플 데이터 생성
+# Dateeframe이란, pandas라이브러리에서 제공하는 2차원 데이터  구조(액셀과 유사)
+dateframe = pd.Dateframe({
+  'first column': ['kor''eng''math''science'],
+  'second column': [10,20,30,40]
+})
+
+#다운로드 버튼 연결
+st.download_button(
+    label='CSV로 성적표 다운로드',
+    data=dataframe.to_csv(),   # dataframe을 csv 형태로 변환
+    file_name_='sample.csv',
+    mime='text/csv'            #데이터 유형
+)
