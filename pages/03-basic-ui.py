5bfa13540b62bcfdf5f4b40be279fc22bd668066
minjae1277 <22073@dongdaemun.ms.kr>
2024-09-30 15:21:50 +0900

    Create 03-basic-ui.py

---
+++ pages/03-basic-ui.py
@@ -0,0 +1,9 @@
+import stremlit as st
+import pandas as pd
+from datetime import datetime as dt
+import datetime
+
+# 버튼 클릭
+button = st.button('버튼을 눌러보세요')
+if button:
+  st.write(':blue[버튼}이 눌렸습니다 :sparkles:')
